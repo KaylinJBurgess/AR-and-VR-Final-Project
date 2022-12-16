3fb594017ede6e01d831de3ad0230f8c0812561a
KaylinJBurgess <kaylinburgess965@gmail.com>
2022-12-15 20:35:09 -0500

    first commit

---
+++ Assets/Avatar/PlayerController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    public Animator anim;
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("vertical", Input.GetAxis("Vertical"));
+        anim.SetFloat("horizontal", Input.GetAxis("Horizontal"));
+
+    }
+}
